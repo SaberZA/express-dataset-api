149f02099a659381a90d8d3ba76f4ce6246b643c
Steven van der Merwe <steven.vandermerwe@kaleidocode.co.za>
2016-05-21 17:32:19 +0200

    Added new frontend

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ var express = require('express');
 var app = express();
 
 var PORT = 3000;
+app.locals.basedir = __dirname;
 
 app.use('/data',express.static('data'));
+app.use('/public', express.static('public'));
+
+app.set('view engine','pug');
 
 app.get('/', function(req,res) {
-    res.send('Hello World!');
+    res.render('index', {title: 'Express Dataset Api', label: 'Express Datasets'});
 });
 
 
